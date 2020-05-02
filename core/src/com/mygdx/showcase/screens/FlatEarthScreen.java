1fc44dc026ff6c2dca827f6dc9678e6aa826e870
wilkon <tb.wilkon@gmail.com>
2020-05-02 03:24:44 -0400

    FlatEarthScreen => added earth and touchpad

--- core/src/com/mygdx/showcase/screens/FlatEarthScreen.java
+++ core/src/com/mygdx/showcase/screens/FlatEarthScreen.java
@@ -3,11 +3,11 @@ package com.mygdx.showcase.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.mygdx.showcase.ShowcaseGame;
@@ -27,11 +27,20 @@ public class FlatEarthScreen implements Screen {
     public void show() {
         Gdx.input.setInputProcessor(stage);
 
+        TextureAtlas atlas = new TextureAtlas(Gdx.files.internal("flat-earth/flat-earth-ui.atlas"));
+        TextureRegion background = atlas.findRegion("earth");
+        Image image = new Image(background);
+        image.setSize(Gdx.graphics.getWidth(),Gdx.graphics.getWidth());
+        image.setPosition(0,Gdx.graphics.getHeight()-image.getHeight());
+        stage.addActor(image);
+
         Table layout = new Table();
         layout.setFillParent(true);
         stage.addActor(layout);
 
         Skin flatEarth = new Skin(Gdx.files.internal("flat-earth/flat-earth-ui.json"));
+
+        Label title = new Label("Flat-Earth-UI", flatEarth);
 
         TextButton backButton = new TextButton("Back", flatEarth);
         backButton.addListener(new ChangeListener(){
@@ -41,7 +50,12 @@ public class FlatEarthScreen implements Screen {
             }
         });
 
-        layout.add(backButton);
+        Touchpad touchpad = new Touchpad(0, flatEarth);
+
+        layout.add(title);
+        layout.row().pad(10, 10,10, 10);
+        layout.add(backButton).pad(0, 0, 0, 10);
+        layout.add(touchpad);
     }
 
     @Override
